d1bc4b16eefad5d21c3af2decca4b3a0c64d5c76
Jack Murray <jack@c0rporation.com>
2013-04-27 20:02:25 +0100

    Unit test project.

---
+++ UnitTest/LibSecurity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibSecurity;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class LibSecurity
+    {
+        [TestMethod]
+        public void TestKeyGeneration()
+        {
+            var key = KeyManager.Create();
+            key.WriteKeyToFile("key.txt");
+        }
+    }
+}
