9f9508c3803186279973edfd41ba2180efc56afd
Jack Murray <jack@c0rporation.com>
2013-04-28 00:39:04 +0100

    add libutil

---
+++ LibUtil/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibUtil
+{
+    public static class Util
+    {
+        public static string BytesToHexString(byte[] data)
+        {
+            return BitConverter.ToString(data).Replace("-", String.Empty).ToLower();
+        }
+    }
+}
