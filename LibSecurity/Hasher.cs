5ea3ed3a7c43e96e50c2484695fd448ba5edcb49
Jack Murray <jack@c0rporation.com>
2013-04-28 10:32:25 +0100

    libsec hasher

---
+++ LibSecurity/Hasher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using LibTrace;
+using LibConfig;
+using LibUtil;
+
+namespace LibSecurity
+{
+    public class Hasher
+    {
+        private static Trace Log = new Trace("LibSecurity");
+        private HashAlgorithm hasher;
+
+        private Hasher(HashAlgorithm hasher)
+        {
+            this.hasher = hasher;
+        }
+
+        public static Hasher Create()
+        {
+            string confHashAlgorithm = Config.Get(Config.HASH_ALGORITHM);
+            HashAlgorithm hasher = HashAlgorithm.Create(confHashAlgorithm);
+            if (hasher == null)
+            {
+                Log.Critical("Unable to find hash algorithm: " + confHashAlgorithm);
+                throw new CryptographicException("Unable to find hash algorithm: " + confHashAlgorithm);
+            }
+
+            return new Hasher(hasher);
+        }
+
+        public byte[] Hash(byte[] data)
+        {
+            return hasher.ComputeHash(data);
+        }
+
+        public string HashBase64(byte[] data)
+        {
+            return Util.BytesToBase64String(Hash(data));
+        }
+    }
+}
