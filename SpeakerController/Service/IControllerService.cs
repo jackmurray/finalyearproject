f5f6610aeeb0389196dcf686f4940f3b8c89bb7d
Jack Murray <jack@c0rporation.com>
2013-05-26 11:53:26 +0100

    add dummy controller svc

---
+++ SpeakerController/Service/IControllerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace SpeakerController
+{
+    [ServiceContract]
+    interface IControllerService
+    {
+        [OperationContract]
+        int GetVersion();
+
+        [OperationContract]
+        byte[] GetCurrentDataKey();
+    }
+}
