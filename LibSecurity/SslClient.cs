fab8f7828443f9fbbeb97b4585ec80cdc92bc5e0
Jack Murray <jack@c0rporation.com>
2013-06-04 22:40:26 +0100

    add ssl client/server classes

---
+++ LibSecurity/SslClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LibSecurity
+{
+    public class SslClient
+    {
+        private X509Certificate2 _cert;
+
+        public SslClient(X509Certificate2 cert)
+        {
+            _cert = cert;
+        }
+
+        public int Connect(int port)
+        {
+            var sock = new TcpClient();
+            sock.Connect(new IPEndPoint(IPAddress.Parse("10.0.1.6"), port));
+            SslStream ssl = new SslStream(sock.GetStream(), false, (a,b,c,d) => true);
+            ssl.AuthenticateAsClient("d3cb375c-b626-4b24-bc3f-f022b12b3f2f", new X509Certificate2Collection(_cert), System.Security.Authentication.SslProtocols.Tls, false);
+            //ssl.WriteByte(0); //work around bug in mono. it tries to read from the stream as part of the AuthenticateAsServer() call
+                              //so we give it a byte to chomp before it can return and start operating normally.
+            int read = ssl.ReadByte();
+            ssl.Close();
+            return read;
+        }
+    }
+}
---
+++ LibSecurity/SslServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LibSecurity
+{
+    public class SslServer
+    {
+        private X509Certificate2 _cert;
+
+        public SslServer(X509Certificate2 cert)
+        {
+            _cert = cert;
+        }
+
+        public void Listen(int port)
+        {
+            var sock = new TcpListener(IPAddress.Any, port);
+            sock.Start();
+            SslStream ssl = new SslStream(sock.AcceptTcpClient().GetStream(), false, (a, b, c, d) => true);
+            ssl.AuthenticateAsServer(_cert, true, System.Security.Authentication.SslProtocols.Tls, false);
+            Console.WriteLine("Accepted SSL connection.");
+            ssl.WriteByte(0xff);
+            ssl.Flush();
+            ssl.Close();
+        }
+    }
+}
