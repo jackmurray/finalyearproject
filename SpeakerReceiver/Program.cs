01042aef958fdba24e91f9d5eaf519bdf53b1df6
Jack Murray <jack@c0rporation.com>
2013-06-06 21:13:35 +0100

    more wcf stuff we don't need any more

--- SpeakerReceiver/Program.cs
+++ SpeakerReceiver/Program.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
-using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
-using System.ServiceModel;
 using LibSecurity;
 using LibTrace;
 using LibConfig;
