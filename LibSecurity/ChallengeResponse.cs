d393659528e5778d1310b4b0315ed7f335f05b29
Jack Murray <jack@c0rporation.com>
2013-09-17 19:21:48 +0100

    add challengeresponse code

---
+++ LibSecurity/ChallengeResponse.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace LibSecurity
+{
+    public class ChallengeResponse
+    {
+        public const int CHALLENGE_SIZE = 32;
+        public const int ROUNDS = 100000;
+        private static RandomNumberGenerator rand = new RNGCryptoServiceProvider();
+
+        public byte[] ChallengeBytes { get; private set; }
+
+        /// <summary>
+        /// Create a new ChallengeResponse object with a random challenge value.
+        /// </summary>
+        public ChallengeResponse()
+        {
+            ChallengeBytes = new byte[CHALLENGE_SIZE];
+            rand.GetBytes(ChallengeBytes);
+        }
+
+        /// <summary>
+        /// Create a ChallengeResponse object with a known challenge value.
+        /// </summary>
+        /// <param name="challenge"></param>
+        public ChallengeResponse(byte[] challenge)
+        {
+            if (challenge == null || challenge.Length != CHALLENGE_SIZE)
+                throw new ArgumentException("Challenge must be of length " + CHALLENGE_SIZE);
+
+            ChallengeBytes = challenge;
+        }
+
+        /// <summary>
+        /// Sign a challenge using the given key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public byte[] Sign(byte[] key)
+        {
+            return CalculateSignature(key);
+        }
+
+        /// <summary>
+        /// Verify the signature on a challenge with the given key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool Verify(byte[] key, byte[] signature)
+        {
+            return signature.SequenceEqual(CalculateSignature(key));
+        }
+
+        private byte[] CalculateSignature(byte[] key)
+        {
+            HMACSHA256 hash = new HMACSHA256(key);
+            byte[] output = new byte[hash.HashSize/8];
+            output = hash.ComputeHash(ChallengeBytes);
+            for (int i = 0; i < ROUNDS; i++) //we do one initial round to mix in the challenge bytes, then repeat the hash ROUNDS times.
+            {
+                output = hash.ComputeHash(output);
+            }
+
+            return output;
+        }
+    }
+}
--- UnitTest/LibSecurity.cs
+++ UnitTest/LibSecurity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LibSecurity;
 
@@ -17,6 +18,22 @@ namespace UnitTest
         public void TestGenerateCert()
         {
             CertManager.Create(KeyManager.Create());
+        }
+
+        [TestMethod]
+        public void TestChallengeResponse()
+        {
+            ChallengeResponse cr = new ChallengeResponse();
+            var key = System.Text.Encoding.UTF8.GetBytes("KEY");
+            byte[] sig = cr.Sign(key);
+            Assert.IsTrue(cr.Verify(key, sig));
+
+            var cr2 = new ChallengeResponse();
+            Assert.IsFalse(cr2.Verify(key, sig)); //will fail because the challenge is different now.
+
+            for (int i = 0; i < sig.Length; i++)
+                sig[i] = 0x00;
+            Assert.IsFalse(cr.Verify(key, sig)); //fails because the sig is invalid now
         }
     }
 }
