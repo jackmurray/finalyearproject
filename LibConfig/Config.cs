debebde57d026ac1d0a5c4f4c65bf6f6807968d4
Jack Murray <jack@c0rporation.com>
2013-10-28 19:37:35 +0000

    add config key for rtp stream buffering time

--- LibConfig/Config.cs
+++ LibConfig/Config.cs
@@ -25,6 +25,7 @@ namespace LibConfig
         public const string TRACE_LEVEL = "traceLevel";
         public const string PAIRING_KEY = "pairingKey"; //This must be the same on all devices.
         public const string MAX_TIMEDIFF = "maxTimeDifference";
+        public const string STREAM_BUFFER_TIME = "streamBufferTime"; //The number of seconds of audio data to pre-buffer.
 
         public static bool IsRunningOnMono { get; private set; }
 
