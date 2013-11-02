0e53d5cc467d108f13315ac987b5f9b070eef282
Jack Murray <jack@c0rporation.com>
2013-11-02 16:13:17 +0000

    added basic RTP input stream code

---
+++ LibTransport/RTPInputStream.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace LibTransport
+{
+    public class RTPInputStream : RTPStreamBase
+    {
+        public RTPInputStream(IPEndPoint ep) : base(ep)
+        {
+            c.Client.Bind(new IPEndPoint(IPAddress.Any, ep.Port));
+        }
+
+        public void SetReceiveTimeout(int ms)
+        {
+            c.Client.ReceiveTimeout = ms;
+        }
+
+        public RTPPacket Receive()
+        {
+            IPEndPoint remoteHost = null;
+            byte[] data = c.Receive(ref remoteHost);
+            return RTPPacket.Parse(data);
+        }
+    }
+}
