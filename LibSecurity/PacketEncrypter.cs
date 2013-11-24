0be9b25b48af39a9f2f2951f7fcfe75988b8b6d1
Jack Murray <jack@c0rporation.com>
2013-11-24 15:56:25 +0000

    encrypter written and tested for exact-blocksize input

--- LibSecurity/PacketEncrypter.cs
+++ LibSecurity/PacketEncrypter.cs
@@ -35,6 +35,11 @@ namespace LibSecurity
             cipher.Init(true, new ParametersWithIV(this.key, iv));
         }
 
+        /// <summary>
+        /// Create a new instance for encryption. Nonce will be generated with CSPRNG.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="ctr"></param>
         public PacketEncrypter(byte[] key, long ctr)
         {
             RandomNumberGenerator rand = new RNGCryptoServiceProvider();
@@ -44,7 +49,7 @@ namespace LibSecurity
         }
 
         /// <summary>
-        /// For decryption, when the nonce must be specified (as transmitted by the controller).
+        /// Create a new instance for encryption or decryption with the specified nonce.
         /// </summary>
         /// <param name="key"></param>
         /// <param name="ctr"></param>
@@ -52,6 +57,28 @@ namespace LibSecurity
         public PacketEncrypter(byte[] key, long ctr, byte[] nonce)
         {
             this.Init(key, nonce, ctr);
+        }
+
+        public byte[] Encrypt(byte[] data)
+        {
+            int realDataLength = data.Length;
+            //BC has a bug that forces you to round this up to the nearest blocksize multiple. because we're in CTR mode we only really need a buffer the same size as our data...
+            int bufSize = realDataLength;
+            int diff = realDataLength%cipher.GetBlockSize();
+            bufSize += diff != 0 ? (cipher.GetBlockSize() - diff) : 0; 
+            byte[] output = new byte[bufSize];
+            int bytesLeft = data.Length;
+
+            //will process only a whole number of blocks. there can be 0 - 15 bytes left unprocessed after this call.
+            bytesLeft -= cipher.ProcessBytes(data, output, 0);
+
+            if (bytesLeft == 0)
+                return output;
+            else
+            {
+                cipher.DoFinal(output, realDataLength - bytesLeft);
+            }
+            return output.Take(realDataLength).ToArray();
         }
     }
 }
--- UnitTest/LibSecurity.cs
+++ UnitTest/LibSecurity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LibSecurity;
@@ -8,6 +9,10 @@ namespace UnitTest
     [TestClass]
     public class LibSecurity
     {
+        private readonly byte[] NIST_DATA1 = {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a};
+
+        private readonly byte[] NIST_RESULT1 = {0x87, 0x4d, 0x61, 0x91, 0xb6, 0x20, 0xe3, 0x26, 0x1b, 0xef, 0x68, 0x64, 0x99, 0x0d, 0xb6, 0xce};
+
         [TestMethod]
         public void TestGenerateKey()
         {
@@ -34,6 +39,24 @@ namespace UnitTest
             for (int i = 0; i < sig.Length; i++)
                 sig[i] = 0x00;
             Assert.IsFalse(cr.Verify(key, sig)); //fails because the sig is invalid now
+        }
+
+        private PacketEncrypter SetupEncrypter()
+        {
+            //this is the NIST test data for AES-CTR
+            byte[] key = new byte[] { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c, };
+            byte[] nonce = new byte[] { 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7 };
+            long ctr = -506097522914230529; //0xf8f9fafbfcfdfeff in signed long
+            return new PacketEncrypter(key, ctr, nonce);
+        }
+
+        [TestMethod]
+        public void TestPacketCryptoExactBlock()
+        {
+            PacketEncrypter enc = SetupEncrypter();
+            byte[] output = enc.Encrypt(NIST_DATA1);
+            Assert.IsTrue(
+                output.SequenceEqual(NIST_RESULT1));
         }
     }
 }
