21ec271b15f96e5f310fd7d72e367adc74cb6ea6
Jack Murray <jack@c0rporation.com>
2013-12-06 16:07:51 +0000

    extended PEKM with the ability to set a new key and then rotate to it

--- LibSecurity/PacketEncrypterKeyManager.cs
+++ LibSecurity/PacketEncrypterKeyManager.cs
@@ -10,8 +10,12 @@ namespace LibSecurity
         public byte[] Key { get; protected set; }
         public byte[] Nonce { get; protected set; }
 
+        private byte[] nextKey, nextNonce;
+
         public const int KEY_LENGTH = 16;
         public const int NONCE_LENGTH = 8; //hardcoded to AES128 values.
+
+        private LibTrace.Trace Log = LibTrace.Trace.GetInstance("LibSecurity");
 
         public PacketEncrypterKeyManager()
         {
@@ -31,21 +35,42 @@ namespace LibSecurity
             this.Key = key;
             this.Nonce = nonce;
 
-            LibTrace.Trace.GetInstance("LibSecurity").Verbose("PEKM: Creating new instance with specified key/nonce.");
+            Log.Verbose("PEKM: Creating new instance with specified key/nonce.");
         }
 
         public byte[] GenerateNewKey()
         {
             Key = CryptRandom.GetBytes(KEY_LENGTH);
-            LibTrace.Trace.GetInstance("LibSecurity").Verbose("PEKM: Generated new key");
+            Log.Verbose("PEKM: Generated new key");
             return Key;
         }
 
         public byte[] GenerateNewNonce()
         {
             Nonce = CryptRandom.GetBytes(NONCE_LENGTH);
-            LibTrace.Trace.GetInstance("LibSecurity").Verbose("PEKM: Generated new nonce");
+            Log.Verbose("PEKM: Generated new nonce");
             return Nonce;
+        }
+
+        public void SetNextKey(byte[] key, byte[] nonce)
+        {
+            this.nextKey = key;
+            this.nextNonce = nonce;
+            Log.Verbose("PEKM: setting next key");
+        }
+
+        public void UseNextKey()
+        {
+            if (nextKey == null || nextNonce == null)
+            {
+                Log.Error("PEKM: Was asked to switch to next key but one has not been set!");
+                return;
+            }
+            this.Key = nextKey;
+            this.Nonce = nextNonce;
+            Log.Verbose("PEKM: switching to next key");
+            nextKey = null;
+            nextNonce = null;
         }
     }
 }
