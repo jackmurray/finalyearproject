df67ae6897f02adba53f0b79ffc855c80245b8a3
Jack Murray <jack@c0rporation.com>
2013-12-19 21:02:57 +0000

    add ConfigState class

---
+++ LibConfig/ConfigState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibConfig
+{
+    public class ConfigState
+    {
+        public ConfigStateFlags Flags { get; protected set; }
+
+        protected ConfigState(ConfigStateFlags f)
+        {
+            this.Flags = f;
+        }
+
+        public static ConfigState GetConfigState()
+        {
+            ConfigStateFlags st = new ConfigStateFlags();
+            if (Config.GetFlag(Config.ENABLE_ENCRYPTION))
+                st |= ConfigStateFlags.EncryptionEnabled;
+            if (Config.GetFlag(Config.ENABLE_AUTHENTICATION))
+                st |= ConfigStateFlags.AuthenticationEnabled;
+
+            return new ConfigState(st);
+        }
+    }
+
+    [Flags]
+    public enum ConfigStateFlags
+    {
+        EncryptionEnabled = 1,
+        AuthenticationEnabled = 2
+    }
+}
