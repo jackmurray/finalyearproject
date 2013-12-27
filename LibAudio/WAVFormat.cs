b43a7f32be780c047cd2c9f9b3bb750a6e77a8d6
Jack Murray <jack@c0rporation.com>
2013-12-27 16:27:07 +0000

    bump .NET version to 4.5 for LibAudio and implement part of Parse() for WAVs

--- LibAudio/WAVFormat.cs
+++ LibAudio/WAVFormat.cs
@@ -8,12 +8,19 @@ namespace LibAudio
 {
     public class WAVFormat : AudioFileReader, IAudioFormat
     {
+        /// <summary>
+        /// Since WAV doesn't have frames, we aim to read up to FRAME_LENGTH_TARGET bytes worth of samples
+        /// and pretend that that's a frame instead. The actual frame size is determined during Parse()
+        /// once we know the Frequency and Sample Size.
+        /// </summary>
+        private const ushort FRAME_LENGTH_TARGET = 1400;
+
         private static readonly byte[] MAGIC_RIFF = {0x52, 0x49, 0x46, 0x46}; //ASCII 'RIFF'
         private static readonly byte[] MAGIC_WAVE = {0x57, 0x41, 0x56, 0x45}; //ASCII 'WAVE'
         private static readonly byte[] MAGIC_FORMAT = {0x66, 0x6D, 0x74, 0x20}; //ASCII 'fmt ' (space)
+        private static readonly byte[] MAGIC_DATA = {0x64, 0x61, 74, 0x61}; //ASCII 'data'
 
-        private uint Frequency, BitRate;
-        private ushort NumChannels, BitsPerSample;
+        private WavFmtHeader FmtHeader;
 
         public WAVFormat(Stream s) : base(s)
         {
@@ -21,7 +28,30 @@ namespace LibAudio
 
         public void Parse()
         {
-            throw new NotImplementedException();
+            //we start out with the stream position ready to read the first byte after the RIFF header.
+            //hopefully that's the FMT header, but if not just read until we get it.
+
+            while (!CheckBytes(MAGIC_FORMAT))
+            {
+                if (EndOfFile())
+                    throw new FormatException("Failure parsing WAV. Hit EOF while searching for FORMAT header.");
+                Skip(1);
+            }
+
+            using (BinaryReader bin = new BinaryReader(_s, Encoding.ASCII, true))
+            {
+                this.FmtHeader = new WavFmtHeader()
+                    {
+                        Magic = bin.ReadChars(4),
+                        size = bin.ReadUInt32(),
+                        Format = bin.ReadUInt16(),
+                        NumChannels = bin.ReadUInt16(),
+                        SampleRate = bin.ReadUInt32(),
+                        ByteRate = bin.ReadUInt32(),
+                        BlockAlign = bin.ReadUInt16(),
+                        BitsPerSample = bin.ReadUInt16()
+                    };
+            }
         }
 
         /// <summary>
@@ -37,7 +67,8 @@ namespace LibAudio
             Skip(8); //skip the magic we just read, and the next field (file size) that we don't care about
             if (!CheckBytes(MAGIC_WAVE))
                 return false;
-
+            Skip(4); //at this point we know it's a WAV and that Parse() will get called next, so push the
+            //position forward past the RIFF header ready to work on the others.
             return true;
         }
 
@@ -53,7 +84,21 @@ namespace LibAudio
 
         public void SeekToStart()
         {
-            throw new NotImplementedException();
+            this._s.Position = 0;
+            this.Parse();
         }
+    }
+
+    class WavFmtHeader
+    {
+        //taken from https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+        public char[] Magic;
+        public uint size;
+        public ushort Format;
+        public ushort NumChannels;
+        public uint SampleRate;
+        public uint ByteRate;
+        public ushort BlockAlign;
+        public ushort BitsPerSample;
     }
 }
