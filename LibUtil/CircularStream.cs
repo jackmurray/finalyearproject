9f1d20aa7de53804115f7d9bfe0f2c92d42ed5e5
Jack Murray <jack@c0rporation.com>
2013-12-30 23:41:42 +0000

    simple circular buffer implementation

---
+++ LibUtil/CircularStream.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LibUtil
+{
+    public class CircularStream : Stream
+    {
+        public byte[] Buffer { get; private set; }
+        private long head = 0, tail = 0;
+        private int capacity;
+
+        public CircularStream(int capacity = 1048576) //default = 1MiB
+        {
+            this.capacity = capacity;
+            Buffer = new byte[capacity];
+        }
+
+        public override void Flush()
+        {
+            return;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    if (offset < 0 || offset > capacity)
+                        throw new ArgumentOutOfRangeException();
+                    
+                    head = offset;
+                    break;
+                case SeekOrigin.Current:
+                    if ((head + offset > capacity) || (head + offset < 0))
+                        throw new ArgumentOutOfRangeException();
+
+                    head += offset;
+                    break;
+                case SeekOrigin.End:
+                    if ((head - offset < 0) || (head - offset > capacity))
+                        throw new ArgumentOutOfRangeException();
+
+                    head = capacity - offset;
+                    break;
+            }
+
+            return head;
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] dest, int offset, int count)
+        {
+            for (int i = 0; i < count; i++, offset++, head++)
+            {
+                if (head == capacity)
+                    head = 0;
+                dest[offset] = Buffer[head];
+            }
+
+            return count;
+        }
+
+        public override void Write(byte[] source, int offset, int count)
+        {
+            for (int i = 0; i < count; i++, offset++, tail++)
+            {
+                if (tail == capacity)
+                    tail = 0;
+                Buffer[tail] = source[offset];
+            }
+        }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return true; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        public override long Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Position { get { return head; } set { head = value; } }
+    }
+}
--- UnitTest/LibUtil.cs
+++ UnitTest/LibUtil.cs
@@ -19,5 +19,25 @@ namespace UnitTest
             result = Util.HexStringToBytes("0001020304");
             Assert.IsTrue(result.SequenceEqual(new byte[] {0x00, 0x01, 0x02, 0x03, 0x04}));
         }
+
+        [TestMethod]
+        public void TestCircularStream()
+        {
+            CircularStream s = new CircularStream(10);
+            s.Write(new byte[] {0x01, 0x01, 0x01, 0x01, 0x01}, 0, 5);
+            Assert.IsTrue(s.Buffer.SequenceEqual(new byte[] { 0x01, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00 }));
+            s.Write(new byte[] { 0x01, 0x01, 0x01, 0x01, 0x01 }, 0, 5);
+            Assert.IsTrue(s.Buffer.SequenceEqual(new byte[] { 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01 }));
+            s.Write(new byte[] { 0x02 }, 0, 1);
+            Assert.IsTrue(s.Buffer.SequenceEqual(new byte[] { 0x02, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01 }));
+
+            byte[] buf = new byte[5];
+            s.Read(buf, 0, 5);
+            Assert.IsTrue(buf.SequenceEqual(new byte[] { 0x02, 0x01, 0x01, 0x01, 0x01}));
+            s.Read(buf, 0, 5);
+            Assert.IsTrue(buf.SequenceEqual(new byte[] { 0x01, 0x01, 0x01, 0x01, 0x01 }));
+            s.Read(buf, 0, 5);
+            Assert.IsTrue(buf.SequenceEqual(new byte[] { 0x02, 0x01, 0x01, 0x01, 0x01 }));
+        }
     }
 }
