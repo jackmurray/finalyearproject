677cc43dba624bc249d23f632551f610ddfc222d
Jack Murray <jack@c0rporation.com>
2013-12-30 22:40:39 +0000

    update the unit tests

--- UnitTest/LibAudio.cs
+++ UnitTest/LibAudio.cs
@@ -14,11 +14,11 @@ namespace UnitTest
         [TestMethod]
         public void TestID3Parse()
         {
-            ID3Tag t = new ID3Tag(File.OpenRead("D:\\Music\\Lacuna Coil\\Dark Adrenaline\\End of Time.mp3"));
+            ID3Tag t = new ID3Tag(new AudioFileReader(File.OpenRead("D:\\Music\\Lacuna Coil\\Dark Adrenaline\\End of Time.mp3")));
             t.Parse();
             Assert.AreEqual(t.Size, (uint)0x08C6);
 
-            t = new ID3Tag(File.OpenRead("D:\\Music\\Icon For Hire\\Scripted\\The Grey.mp3"));
+            t = new ID3Tag(new AudioFileReader(File.OpenRead("D:\\Music\\Icon For Hire\\Scripted\\The Grey.mp3")));
             t.Parse();
             Assert.AreEqual(t.Size, (uint)0x017ED5);
         }
@@ -26,21 +26,20 @@ namespace UnitTest
         [TestMethod]
         public void MP3WithID3Header()
         {
-            Stream s = File.OpenRead("D:\\Music\\Lacuna Coil\\Dark Adrenaline\\End of Time.mp3");
+            AudioFileReader s = new AudioFileReader(File.OpenRead("D:\\Music\\Lacuna Coil\\Dark Adrenaline\\End of Time.mp3"));
             ID3Tag t = new ID3Tag(s);
             t.Parse();
             MP3Format mp3 = new MP3Format(s);
-            mp3.Skip((int)t.Size);
+            s.Skip((int)t.Size);
             mp3.Parse();
             Assert.AreEqual(mp3.BitRate, 192000);
             Assert.AreEqual(mp3.Frequency, 44100);
-            s.Close();
 
-            s = File.OpenRead("D:\\Music\\Icon For Hire\\Scripted\\The Grey.mp3");
+            s = new AudioFileReader(File.OpenRead("D:\\Music\\Icon For Hire\\Scripted\\The Grey.mp3"));
             t = new ID3Tag(s);
             t.Parse();
             mp3 = new MP3Format(s);
-            mp3.Skip((int)t.Size);
+            s.Skip((int)t.Size);
             mp3.Parse();
             Assert.AreEqual(mp3.BitRate, 320000);
             Assert.AreEqual(mp3.Frequency, 44100);
