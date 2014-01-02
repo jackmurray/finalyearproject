eb2fe729e59b9cec334e30c60fb5819854589131
Jack Murray <jack@c0rporation.com>
2014-01-02 22:06:31 +0000

    add ControllerState class to track the state of the controller

---
+++ LibCommon/ControllerState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace LibCommon
+{
+    public class ControllerState
+    {
+        public StreamMode Mode { get; set; }
+        public bool EncryptionEnabled { get; set; }
+        public bool AuthenticationEnabled { get; set; }
+    }
+
+    public enum StreamMode
+    {
+        File,
+        Loopback
+    }
+}
