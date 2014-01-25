ff2b7283710140037f70b5acc09aa4b51381bc69
Jack Murray <jack@c0rporation.com>
2014-01-25 13:26:08 +0000

    change SR to use SDL-based playback rather than our own timing

--- SpeakerReceiver/StreamReceiver.cs
+++ SpeakerReceiver/StreamReceiver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using LibSecurity;
@@ -16,12 +17,20 @@ namespace SpeakerReceiver
         private object syncLock = new object();
         private RTPInputStream s;
         private bool shouldRunReceiver = true, shouldRunPlayer = true;
-        private Thread receiveThread, playerThread;
+        private Thread receiveThread;
+
         private DateTime basetime;
+        private ushort SamplesPerFrame;
+        private ushort Frequency;
+        private byte Channels;
+
         private Trace Log = Trace.GetInstance("LibTransport");
         private AudioPlayer player = null;
         private Dictionary<int, RTPPacket> Buffer = new Dictionary<int, RTPPacket>();
         private int i = 1; //read pointer for the buffer. yes, 1, because the first packet has sequencenumber=1
+        private int packetPos = 0; //position within the current packet, if a partial read is needed to satisfy the player's request.
+        private int dataPacketsBuffered = 0; //count of how many data packets we have. don't want to just use Buffer.Count() because it will include controls too.
+        private int minBufPackets = int.MaxValue; //so we don't need an additional check for 'did we calculate the value yet'
 
         public delegate void NotifyKeyRotation();
         public event NotifyKeyRotation OnKeyRotatePacketReceived;
@@ -40,9 +49,8 @@ namespace SpeakerReceiver
         {
             //only call this method from outside the streamreceiver, because the internal threads are the ones being shut down!
             shouldRunReceiver = false;
-            shouldRunPlayer = false;
 
-            while (receiveThread.IsAlive || playerThread.IsAlive)
+            while (receiveThread.IsAlive)
             {
             }
             return;
@@ -55,8 +63,6 @@ namespace SpeakerReceiver
 
             this.receiveThread = new Thread(ReceiveThreadProc);
             this.receiveThread.Start();
-
-            this.ResetPlayerThread();
         }
 
         public void SetEncryptionKey(PacketEncrypterKeyManager pekm)
@@ -69,12 +75,6 @@ namespace SpeakerReceiver
             this.s.EnableVerification(v);
         }
 
-        private void ResetPlayerThread()
-        {
-            shouldRunPlayer = true;
-            this.playerThread = new Thread(PlayerThreadProc);
-        }
-
         /// <summary>
         /// Call from the player thread to stop it.
         /// </summary>
@@ -84,44 +84,30 @@ namespace SpeakerReceiver
             Buffer.Clear();
         }
 
-        private void HandlePacket(RTPPacket _p)
+        private void HandleControlPacket(RTPControlPacket p)
         {
-            if (_p.Marker) //control packet
+            switch (p.Action)
             {
-                RTPControlPacket p = _p as RTPControlPacket;
-                switch (p.Action)
-                {
-                    case RTPControlAction.Play:
-                        Log.Information("Stream started.");
-                        break;
-                    case RTPControlAction.Stop:
-                        Log.Information("Stop packet received - end of stream.");
-                        this.EndPlayerThread();
-                        break;
-                    case RTPControlAction.Pause:
-                        Log.Information("Pausing stream.");
-                        this.EndPlayerThread();
-                        break;
-                    case RTPControlAction.FetchKey:
-                    case RTPControlAction.SwitchKey:
-                    case RTPControlAction.HeaderSync:
-                        Log.Verbose(
-                            "Fetch/SwitchKey/HeaderSync packet taken from buffer. Don't care as it's already been actioned.");
-                        break;
-                    default:
-                        Log.Warning(String.Format("Control packet received but unable to handle. Type={0} seq={1}", p.Action, p.SequenceNumber));
-                        break;
-                }
-            }
-            else //data packet
-            {
-                if (basetime == null)
-                    return; //if we haven't yet got a basetime we can't proceed processing a data packet.
-
-                lock (player)
-                {
-                    player.Write(_p.Payload);
-                }
+                case RTPControlAction.Play:
+                    Log.Information("Stream started.");
+                    break;
+                case RTPControlAction.Stop:
+                    Log.Information("Stop packet received - end of stream.");
+                    this.EndPlayerThread();
+                    break;
+                case RTPControlAction.Pause:
+                    Log.Information("Pausing stream.");
+                    this.EndPlayerThread();
+                    break;
+                case RTPControlAction.FetchKey:
+                case RTPControlAction.SwitchKey:
+                case RTPControlAction.HeaderSync:
+                    Log.Verbose(
+                        "Fetch/SwitchKey/HeaderSync packet taken from buffer. Don't care as it's already been actioned.");
+                    break;
+                default:
+                    Log.Warning(String.Format("Control packet received but unable to handle. Type={0} seq={1}", p.Action, p.SequenceNumber));
+                    break;
             }
         }
 
@@ -140,24 +126,25 @@ namespace SpeakerReceiver
                         var cp = p as RTPControlPacket;
                         if (cp.Action == RTPControlAction.Play)
                         {
-                            if (playerThread.ThreadState == ThreadState.Running ||
-                                playerThread.ThreadState == ThreadState.WaitSleepJoin)
+                            if (s.State == StreamState.Started)
                             {
                                 Log.Information("Got a Play packet while already playing. Ignoring it.");
                             }
                             else
                             {
-                                this.basetime = (cp as RTPPlayPacket).baseTime;
+                                var playPacket = (cp as RTPPlayPacket);
+                                this.basetime = playPacket.baseTime;
                                 Log.Verbose("Taking " + basetime + ":" + basetime.Millisecond +
                                             " as the base time stamp.");
-                                if (playerThread.ThreadState == ThreadState.Unstarted)
-                                    //the first time this object is used.
-                                    playerThread.Start();
-                                if (playerThread.ThreadState == ThreadState.Stopped)
-                                {
-                                    this.ResetPlayerThread();
-                                    this.playerThread.Start();
-                                }
+                                this.SamplesPerFrame = playPacket.SamplesPerFrame;
+                                this.Frequency = playPacket.Frequency;
+                                this.Channels = playPacket.Channels;
+
+                                double ratefrac = (double)LibConfig.Config.GetInt(LibConfig.Config.STREAM_BUFFER_TIME)/1000; //fraction of 1 second we want to buffer
+                                double framefrac = Frequency/((double)SamplesPerFrame/Channels); //num of frames to make 1 s
+                                minBufPackets = (int)Math.Ceiling(framefrac*ratefrac);
+
+                                player.Setup(this.Callback, Frequency, Channels);
                             }
                         }
                         else if (cp.Action == RTPControlAction.FetchKey)
@@ -179,18 +166,30 @@ namespace SpeakerReceiver
                         }
                         else if (cp.Action == RTPControlAction.HeaderSync)
                         {
-                            lock (player) //perhaps not strictly necessary but we want to make sure that nothing tries to write at the same time.
-                            {
-                                player.Setup(this.Callback, cp.ExtraData);
-                            }
+                            
                         }
                         else if (cp.Action == RTPControlAction.Pause)
                         {
-                            
+
                         } //don't care about pause as it's not that interesting (basically the same as Stop).
                         else
                         {
                             Log.Warning("Unknown control packet received.");
+                        }
+                    }
+                    else
+                    {
+                        dataPacketsBuffered++;
+
+                        if (s.State != StreamState.Started && s.State != StreamState.Paused && dataPacketsBuffered >= minBufPackets)
+                        {
+                            lock (player)
+                            //perhaps not strictly necessary but we want to make sure that nothing tries to write at the same time.
+                            {
+                                Log.Verbose("Hit our target of " + minBufPackets + ", starting playback.");
+                                this.player.Start();
+                                s.State = StreamState.Started;
+                            }
                         }
                     }
 
@@ -212,79 +211,75 @@ namespace SpeakerReceiver
             }
         }
 
-        /// <summary>
-        /// Code executed by the audio player thread. This thread isn't started until there is at least one packet in the buffer.
-        /// </summary>
-        private void PlayerThreadProc()
-        {
-            i = 1;
-            int maxAllowedError = LibConfig.Config.GetInt(LibConfig.Config.MAX_STREAM_ERROR); //in millisec
-            int streamBufferTime = LibConfig.Config.GetInt(LibConfig.Config.STREAM_BUFFER_TIME); //in millisec
-            double totalError = 0;
-            while (shouldRunPlayer)
-            {
-                RTPPacket p = null;
-
-                while (p == null)
-                {
-                    //this section uses Monitor directly instead of the lock statement because we don't want to sleep holding the lock or have to acquire it twice
-                    Monitor.Enter(syncLock);
-                    if (Buffer.ContainsKey(i))
-                    {
-                        p = Buffer[i];
-                        Buffer.Remove(i);
-                        Monitor.Exit(syncLock);
-                        i++;
-                    }
-                    else
-                    {
-                        if (!shouldRunPlayer)
-                        {
-                            Monitor.Exit(syncLock);
-                            return;
-                        }
-                        Log.Warning("Receiver buffer underrun!");
-                        /*Log.Warning(String.Format("It is currently {0} and the last packet we got is for {1}",
-                                                  LibUtil.Util.FormatDate(DateTime.UtcNow),
-                                                  LibUtil.Util.FormatDate(RTPPacket.BuildDateTime(Buffer[i - 1].Timestamp, this.basetime))));*/
-                        
-                        Monitor.Exit(syncLock);
-                        Thread.Sleep(streamBufferTime); //maybe after sleeping this thread there'll be some more packets.
-                    }
-                }
-
-                DateTime packetactiontime = RTPPacket.BuildDateTime(p.Timestamp, this.basetime);
-                TimeSpan sleeptime = packetactiontime - DateTime.UtcNow;
-                int sleep = (int) sleeptime.TotalMilliseconds;
-                if (sleep > 0)
-                {
-                    //totalError += actualtime - timersleep;
-
-                    /*if (totalError >= maxAllowedError)
-                    {
-                        //Log.Verbose(totalError + "ms of drift has built up, reducing it by " + maxAllowedError); //disabled for perf
-                        timersleep += maxAllowedError;
-                        totalError -= maxAllowedError;
-                    }*/
-
-                    //if (timersleep > 0)
-                        Thread.Sleep(sleep);
-                }
-
-                HandlePacket(p);
-                //Log.Verbose("Remaining: " + (Buffer.Count - i)); //disabled for perf
-            }
-            player.Reset(); //this thread is terminating, so reset the audio output for the next one.
-        }
-
         public void DeliverNewKey(byte[] key, byte[] nonce)
         {
             this.s.DeliverNewKey(key, nonce);
         }
 
+        /// <summary>
+        /// Callback called by SDL whenever it wants data.
+        /// </summary>
+        /// <param name="user">Unused.</param>
+        /// <param name="buffer">The unmanaged pointer to the start of the buffer</param>
+        /// <param name="length">The length of the buffer we're requested to fill</param>
         public void Callback(IntPtr user, IntPtr buffer, int length)
         {
-            
+            byte[] managedbuf = new byte[length];
+            int managedbufptr = 0;
+
+            while (length > 0)
+            {
+                lock (syncLock)
+                {
+                    if (Buffer.ContainsKey(i))
+                    {
+                        RTPPacket p = Buffer[i];
+                        if (p.Marker)
+                        {
+                            HandleControlPacket(p as RTPControlPacket);
+                            Buffer.Remove(i);
+                            i++;
+                            packetPos = 0;
+                        }
+                        else
+                        {
+                            if ((p.Payload.Length - packetPos) <= length)
+                                //if this packet isn't enough to fully satisfy the request, copy all its contents and loop again
+                            {
+                                Array.Copy(p.Payload, packetPos, managedbuf, managedbufptr, p.Payload.Length - packetPos);
+                                managedbufptr += p.Payload.Length;
+                                length -= p.Payload.Length;
+                                Buffer.Remove(i);
+                                i++;
+                                packetPos = 0;
+                                dataPacketsBuffered--;
+                            }
+                            else //take part of the packet and make a note of how much we used.
+                            {
+                                Array.Copy(p.Payload, 0, managedbuf, managedbufptr, length);
+                                packetPos = length;
+                                managedbufptr += length;
+                                length = 0;
+                                //we don't increment i because we want to get the rest of the packet next time
+                            }
+                        }
+                    }
+                    else //packet not in buffer. zero fill instead
+                    {
+                        Thread.Sleep(1);
+                        int needed = Math.Min(SamplesPerFrame * 4, length);
+                        Array.Clear(managedbuf, managedbufptr, needed);
+                            //TODO: integrate properly with format sample size
+                        managedbufptr += needed;
+                        length -= needed;
+                        i++;
+                        packetPos = 0;
+                        Log.Verbose("Zero-filling " + needed);
+                    }
+                }
+            }
+
+            Marshal.Copy(managedbuf, 0, buffer, managedbuf.Length); //although we don't actually need the lock any more, we don't want any other threads to run until this completes so we won't release it until the copy is done
         }
     }
 }
