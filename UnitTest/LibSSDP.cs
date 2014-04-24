b4ba278a6f0d8dc8f20310dd2aa79381aaa197cf
Jack Murray <jack@c0rporation.com>
2014-04-24 16:07:52 +0100

    added ssdp tests

---
+++ UnitTest/LibSSDP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LibSecurity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using global::LibSSDP;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class LibSSDP
+    {
+        [TestMethod]
+        public void TestPacketConstructAndSerialise()
+        {
+            SSDPPacket p = new SSDPAnnouncePacket();
+            var key = KeyManager.GetKey();
+            var cert = CertManager.GetCert(key);
+            p = new SSDPAnnouncePacket(key, cert);
+            p.Serialize();
+
+            p = new SSDPResponsePacket(key, cert);
+            p.Serialize();
+
+            p = new SSDPSearchPacket();
+            p.Serialize();
+        }
+    }
+}
