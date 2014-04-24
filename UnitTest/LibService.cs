27dce491ee7cf7a0e7274a52b8ba8fe55b9d02e6
Jack Murray <jack@c0rporation.com>
2014-04-24 16:07:57 +0100

    added ssl test

--- UnitTest/LibService.cs
+++ UnitTest/LibService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Net;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
+using LibConfig;
+using LibSecurity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LibService;
 
@@ -53,6 +56,22 @@ Host: example
             Assert.AreEqual(-1, HttpMessage.FindByteSequence(new byte[] { 0x01 }, new byte[] { 0x00 }, 0));
             Assert.AreEqual(2, HttpMessage.FindByteSequence(new byte[] { 0x01, 0x01 }, new byte[] { 0x00, 0x00, 0x01, 0x01 }, 0));
             Assert.AreEqual(-1, HttpMessage.FindByteSequence(new byte[] { 0x01, 0x01 }, new byte[] { 0x00, 0x00, 0x00, 0x01 }, 0));
+        }
+
+        [TestMethod]
+        public void TestSSLConnection()
+        {
+            var key = KeyManager.GetKey();
+            var cert = CertManager.GetCert(key);
+            TrustedKeys.Add(cert.ToDotNetPublicCert()); //make sure we trust ourselves
+            SslServer s = new SslServer(cert.ToDotNetCert(key));
+            s.Listen(10451);
+
+            SslClient c = new SslClient(cert.ToDotNetCert(key));
+            c.Connect(new System.Net.IPEndPoint(IPAddress.Loopback, 10451));
+
+            c.Close();
+            s.Stop();
         }
     }
 }
